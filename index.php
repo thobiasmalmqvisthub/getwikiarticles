e8fa05181c02e032d1497b97fc6d4a185105983b
thobiasmalmqvist <malmqvistt@gmail.com>
2015-01-22 10:26:44 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang = "sv">
+
+<head>
+	<meta charset="utf-8">
+	<title>Programmeringstest Phonera</title>
+	<link rel="stylesheet" href="style/stylesheet.css">
+
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	
+	
+	
+	<script type="text/JavaScript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js">
+			
+			
+	</script>
+	
+</head>	
+
+<body>
+
+	
+	
+		
+
+
+
+
+	<input type="button" value="Hämta"/>
+	
+	<div id="field">		
+		<?php include ('getTitle.php')?>
+	</div>
+
+</body>
+
+</html>
